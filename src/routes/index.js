4625833cd322ba7ed4b51178fea2f908241ea754
Hur <hur.filho@gmail.com>
2023-02-18 13:11:15 -0300

    feat: added status code and header to responses

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,14 @@ const router = express.Router();
 const response = require('../assets/channels.js');
 
 router.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'html');
     res.send('API do Gerenciador de tarefas rodando 🥳')
 });
 
 router.get('/api/channels', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
     res.json(response);
 });
 
