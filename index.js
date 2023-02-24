13f344b816bfa705d9aaf9bd070bc7c2ac3f5992
Hur <hur.filho@gmail.com>
2023-02-24 10:58:53 -0300

    fix: :bug: fixed cors behavior

--- index.js
+++ index.js
@@ -19,18 +19,7 @@ const runServer = (PORT) => {
     console.log(`Server running on port ${PORT}`);
 }
 
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    },
-    credentials: true,
-}
-app.use(cors(corsOptions))
+app.use(cors())
 
 mongoose.set('strictQuery', false);
 
