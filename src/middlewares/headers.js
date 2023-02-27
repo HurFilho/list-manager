98d3883efbb6b060ee4d42b7669fe8cec0650e93
Hur <hur.filho@gmail.com>
2023-02-27 15:19:48 -0300

    fix: :bug: fixed cors behavior

--- src/middlewares/headers.js
+++ src/middlewares/headers.js
@@ -1,6 +1,5 @@
 exports.createHeaders = () => {
     return {
-        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
         'Cache-Control': 's-max-age=1, stale-while-revalidate'
     }
